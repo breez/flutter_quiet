5a39e1cf209fb66dc0820af37c320f00179d4f2b
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-09-10 17:12:02 +0300

    init repo

---
+++ ios/Classes/FlutterQuietPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterQuietPlugin.h"
+
+@implementation FlutterQuietPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_quiet"
+            binaryMessenger:[registrar messenger]];
+  FlutterQuietPlugin* instance = [[FlutterQuietPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
